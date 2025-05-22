83f9e3099f00beacb88c6171e91e7702daee857c
Hamza Aydin <96042394+Hamza-Aydin@users.noreply.github.com>
2025-05-22 16:43:05 +0300

    Add files via upload

---
+++ 2_degiskenler.js
@@ -0,0 +1,14 @@
+console.log(5000*1.3);//haftalık maaş:can
+console.log(6000*1.3);//haftalık maaş:ali
+
+// şimdi bunları değişken tanımlayrak yapalım
+
+var maascan= 5000;
+var maasali= 6000;
+var zamorani=1.3;
+
+console.log(maasali*zamorani,maascan*zamorani);
+
+//büyük harfle başlanmamalı
+//türkçe karakter olmamalı
+//sayı ile başlanmaz
