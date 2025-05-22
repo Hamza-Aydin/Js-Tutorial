ce69a3287870d5e6b8028e7491c9ebd812860aeb
Hamza Aydin <96042394+Hamza-Aydin@users.noreply.github.com>
2025-05-22 16:21:29 +0300

    Add files via upload

---
+++ 1_intro.js
@@ -0,0 +1,5 @@
+console.log("merhaba");
+
+//burada terminal kullanrak da node komutuyla js dosyasını çalıştırabiliri
+//ama bulunduğu konumu tanımlamamız lazım terminale yoksa hata verir(cd yazıp boşlukla dosya konumunu gireceğiz)
+//veya index.html açarak console kısmından görüntüleyebiliriz
