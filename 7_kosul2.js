f07ca6c1a478b298dd4daa9735d8a9f2246dc9bd
Hamza Aydin <96042394+Hamza-Aydin@users.noreply.github.com>
2025-05-23 15:23:00 +0300

    Add files via upload

---
+++ 7_kosul2.js
@@ -0,0 +1,12 @@
+let yas=18;
+let mezuniyet="lise";
+
+if(yas==18 && (mezuniyet=="lise" || mezuniyet=="üniversite")){
+    console.log("ehliyet alabilir");
+}
+else{
+    console.log("ehliyet alamaz!");
+}
+
+//&& and oparetörüdür iki tarafında true olması lazım true olabilmesi için
+//|| ise or oparetörüdür sadece bir taraf true olsa yeterli
